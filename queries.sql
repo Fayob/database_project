37ed19ef92d0d06684a31e36eb1b66865d24d966
fayob <adedejifavour6@gmail.com>
2022-12-06 11:43:10 +0100

    Add more queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+SELECT * FROM animals;
 SELECT * from animals WHERE name = 'Gabumon';
+SELECT name, escape_attempts, weight_kg from animals;
