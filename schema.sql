d8fe9fa0c6e81dd459d6735f3fbf7d644d68fb59
fayob <adedejifavour6@gmail.com>
2022-12-06 22:45:29 +0100

    Add species column to animal table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL
-);
+-- CREATE TABLE animals (
+--     id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+--     name VARCHAR(100),
+--     date_of_birth DATE,
+--     escape_attempts INT,
+--     neutered BOOLEAN,
+--     weight_kg DECIMAL
+-- );
+
+ALTER TABLE animals ADD species VARCHAR(50)
