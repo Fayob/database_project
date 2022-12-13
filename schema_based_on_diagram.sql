95434299477f25f2923b8aec7cb4429c16959dd3
fayob <adedejifavour6@gmail.com>
2022-12-13 21:19:39 +0100

    Move create index into schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,8 +1,11 @@
+-- Create Patients Table
 CREATE TABLE patients (
 	id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   date_of_birth DATE
 );
+
+-- Create Medical Histories Table
 
 CREATE TABLE medical_histories (
 	id SERIAL PRIMARY KEY,
@@ -11,16 +14,22 @@ CREATE TABLE medical_histories (
   status VARCHAR(50)
 );
 
+-- Create Treatment Table
+
 CREATE TABLE treatments (
 	id SERIAL PRIMARY KEY,
   type VARCHAR (50),
   name VARCHAR (100)
 );
 
+-- Create Medical Treatment Table
+
 CREATE TABLE medical_treatment (
 	medical_histories_id INT REFERENCES medical_histories(id),
   treaments_id INT REFERENCES treatments (id)
 );
+
+-- Create Invoices Table
 
 CREATE TABLE invoices (
 	id SERIAL PRIMARY KEY,
@@ -30,6 +39,8 @@ CREATE TABLE invoices (
   medical_history_id INT REFERENCES medical_histories(id) UNIQUE
 );
 
+-- Create Invoice Items Table
+
 CREATE TABLE invoice_items (
 	id SERIAL PRIMARY KEY,
   unit_price DECIMAL,
@@ -38,3 +49,12 @@ CREATE TABLE invoice_items (
   invoice_id INT REFERENCES invoices(id),
 	treatment_id INT REFERENCES treatments(id)
 );
+
+-- Create index for all foreign key
+
+CREATE INDEX ON invoices (medical_history_id);
+CREATE INDEX ON medical_histories (patient_id);
+CREATE INDEX ON medical_treatment (medical_histories_id);
+CREATE INDEX ON medical_treatment (treaments_id);
+CREATE INDEX ON invoice_items (invoice_id);
+CREATE INDEX ON invoice_items (treatment_id);
