3b65a6db09f14dd592dda631318a226bc8b68e20
fayob <adedejifavour6@gmail.com>
2022-12-13 13:57:23 +0100

    Fixed some error in the ids

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,22 +1,21 @@
 CREATE TABLE patients (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   name VARCHAR(100),
   date_of_birth DATE
 );
 
 CREATE TABLE medical_histories (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   admitted_at TIMESTAMP,
   patient_id INT REFERENCES patients(id),
   status VARCHAR(50)
 );
 
 CREATE TABLE treatments (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   type VARCHAR (50),
   name VARCHAR (100)
 );
-
 
 CREATE TABLE medical_treatment (
 	medical_histories_id INT REFERENCES medical_histories(id),
@@ -24,7 +23,7 @@ CREATE TABLE medical_treatment (
 );
 
 CREATE TABLE invoices (
-	id INT PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   total_amount DECIMAL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
@@ -32,7 +31,7 @@ CREATE TABLE invoices (
 );
 
 CREATE TABLE invoice_items (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   unit_price DECIMAL,
   quantity INT,
   total_price DECIMAL,
